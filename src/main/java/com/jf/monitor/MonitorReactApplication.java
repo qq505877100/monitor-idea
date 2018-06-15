0cbe35d04142375804b0c22f17ea095e639db253
fuxianhui <505877100@qq.com>
2018-06-15 17:04:10 +0800

    init project

---
+++ src/main/java/com/jf/monitor/MonitorReactApplication.java
@@ -0,0 +1,12 @@
+package com.jf.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorReactApplication.class, args);
+	}
+}
